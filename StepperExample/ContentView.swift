adc5ae3bb73b51477f070d81f6d835a060d44859
Thomas Noone <tnoone@lcs.on.ca>
2025-02-16 21:37:30 -0500

    Initial Commit

---
+++ StepperExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StepperExample
+//
+//  Created by Thomas Noone on 2025-02-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Thomas Noone on 2025-02-16.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

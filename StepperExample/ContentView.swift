bfe8143d0e2571da2ad6794d26c1dfa74fb78b86
Thomas Noone <tnoone@lcs.on.ca>
2025-02-16 22:39:21 -0500

    Added a stepper control that is bound to the "base" stored property. The user can now click to change the base in the app's interface.

--- StepperExample/ContentView.swift
+++ StepperExample/ContentView.swift
@@ -31,7 +31,10 @@ struct ContentView: View {
                 Text("25")
                     .font(.system(size: 96))
             }
-                        
+            Stepper(value: $base, label: {
+                Text("Base")
+            })
+            
             Spacer()
         }
         .padding()

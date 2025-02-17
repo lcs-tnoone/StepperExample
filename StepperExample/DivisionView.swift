489a43f4c64ec8ce00c06758b8e385a758ceeb2c
Thomas Noone <tnoone@lcs.on.ca>
2025-02-16 23:16:37 -0500

    Finished Division and Multiplication Views

---
+++ StepperExample/DivisionView.swift
@@ -0,0 +1,18 @@
+//
+//  DivisionView.swift
+//  StepperExample
+//
+//  Created by Thomas Noone on 2025-02-16.
+//
+
+import SwiftUI
+
+struct DivisionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DivisionView()
+}

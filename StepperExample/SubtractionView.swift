7e170b2cf3a9996a33632ad50ce9c1ad45c320c9
Thomas Noone <tnoone@lcs.on.ca>
2025-02-16 23:10:56 -0500

    finished subtraction View

---
+++ StepperExample/SubtractionView.swift
@@ -0,0 +1,18 @@
+//
+//  SubtractionView.swift
+//  StepperExample
+//
+//  Created by Thomas Noone on 2025-02-16.
+//
+
+import SwiftUI
+
+struct SubtractionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubtractionView()
+}

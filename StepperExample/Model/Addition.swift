7a3f4aa991cead7a081127a7ff3c01848dce64d7
Thomas Noone <tnoone@lcs.on.ca>
2025-02-22 10:34:24 -0500

    saving work

---
+++ StepperExample/Model/Addition.swift
@@ -0,0 +1,18 @@
+//
+//  Addition.swift
+//  StepperExample
+//
+//  Created by Thomas Noone on 2025-02-22.
+//
+
+import SwiftUI
+
+struct Addition: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Addition()
+}
